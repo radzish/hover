7a348751e64a2c76334d94a45e7e1c2f7b3201ba
radzish <radzish@gmail.com>
2019-05-14 23:17:48 +0300

    Update build.go

--- cmd/build.go
+++ cmd/build.go
@@ -180,7 +180,6 @@ func build(projectName string, targetOS string, vmArguments []string) {
 	cmdGoBuild := exec.Command(goBin, "build",
 		"-o", outputBinaryPath,
 		fmt.Sprintf("-ldflags=-X -H=windowsgui main.vmArguments=%s", strings.Join(vmArguments, ";")),
-		dotSlash+"cmd",
 	)
 	cmdGoBuild.Dir = filepath.Join(wd, "desktop")
 	cmdGoBuild.Env = append(os.Environ(),
